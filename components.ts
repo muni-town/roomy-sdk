d7b0ffb5ea20c332805efd003995f5ef88cb2c28
Zicklag <zicklag@katharostech.com>
2025-03-16 10:47:49 -0500

    feat: add the rest of the Roomy component definitions.

--- components.ts
+++ components.ts
@@ -4,13 +4,51 @@ import {
   LoroMap,
   LoroMovableList,
   LoroText,
+  Marker,
 } from "@muni-town/leaf";
 
-/** The display name, short description, and image / icon URL of an entity. */
+/** The display name, slug, short description, and image / icon URI of an entity. */
 export const BasicMeta = defComponent(
   "name:01JPE9PEBF47QJR6THNW4J4JXJ",
-  LoroMap<{ name: string; description?: string; image?: string }>,
+  LoroMap<{
+    name: string;
+    slug?: string;
+    description?: string;
+    /** The entity ID of an entity with an `ImageUri` component. */
+    image?: EntityIdStr;
+  }>,
   (map) => map.set("name", "")
+);
+
+/** This entity is in reply to another entity. */
+export const ReplyTo = defComponent(
+  "replyTo:01JPFR9RXR4BECAMCEH0M8XPQY",
+  LoroMap<{ entity: EntityIdStr }>
+);
+
+/** Entities with this marker component have been soft deleted and should be shown as deleted
+ * usually in the UI, even though the data is preserved. */
+export const SoftDelete = defComponent(
+  "softDelete:01JPFQK63H9W2RA8Q5GQ19S1DY",
+  Marker
+);
+
+/** An image from a URI. */
+export const ImageUri = defComponent(
+  "imageUri:01JPFRMSYWE2G0JHMS7RW478G3",
+  LoroMap<{ uri: string; width?: number; height?: number; alt?: string }>,
+  (map) => map.set("uri", "example:image")
+);
+
+/**
+ * An ordered list of Entity IDs for images or image {@linkcode Collection}s. Each image should have
+ * an {@linkcode ImageUri} component.
+ *
+ * This might represent images attached to a chat message, or an image gallery or something similar.
+ * */
+export const Images = defComponent(
+  "spaces:01JPE97AYAJSDP0NRAH79S9792",
+  LoroMovableList<EntityIdStr>
 );
 
 /** A list of Entity IDs for Roomy spaces. */
@@ -20,7 +58,7 @@ export const Spaces = defComponent(
 );
 
 /**
- * This entity represents a collection of links to other entities.
+ * This entity represents a generic collection of other entities.
  *
  * This component can be used to implement things like pagination by putting a list of collection
  * entities, the pages, inside, for example, a {@linkcode Messages} component.
@@ -34,8 +72,16 @@ export const Collection = defComponent(
 );
 
 /**
- * An ordered list of Roomy threads. Usually threads are sorted by creation order with the latest
- * ones at the end of the list.
+ * An ordered list of Entity IDs for Roomy channels or channel {@linkcode Collection}s.
+ * */
+export const Channels = defComponent(
+  "channels:01JPFMTA2QAFY2D3CV1C0YBZQ9",
+  LoroMovableList<EntityIdStr>
+);
+
+/**
+ * An ordered list of Roomy threads or thread {@linkcode Collection}s. Usually threads are sorted by
+ * creation order with the latest ones at the end of the list.
  * */
 export const Threads = defComponent(
   "threads:01JPEB05824ZR836QMV6B3E69F",
@@ -43,11 +89,20 @@ export const Threads = defComponent(
 );
 
 /**
- * An ordered list of Entity IDs for Roomy messages. The latest messages are at the end of the list.
+ * An ordered list of Entity IDs for Roomy messages or message {@linkcode Collection}s. The latest
+ * messages are at the end of the list.
  * */
 export const Messages = defComponent(
   "messages:01JPEAH76WBQ4XYZQZT7MQT19V",
   LoroMovableList<EntityIdStr>
+);
+
+/**
+ * The DIDs ( Decentralized Identifiers ) for the authors of the given entity.
+ */
+export const AuthorDids = defComponent(
+  "authorDids:01JPEBV8TJ8YCXXYD156NJSR5Y",
+  LoroMovableList<string>
 );
 
 /**
@@ -62,9 +117,13 @@ export const Content = defComponent(
 );
 
 /**
- * The DIDs ( Decentralized Identifiers ) for the authors of the given entity.
+ * The items in a Roomy space's channel / category list sidebar.
+ *
+ * For now, each item should be a channel or a category entity, i.e. it should either have a
+ * {@linkcode Channels} component ( for a category ) or a {@linkcode Messages} component ( for a
+ * channel ).
  */
-export const AuthorDids = defComponent(
-  "authorDids:01JPEBV8TJ8YCXXYD156NJSR5Y",
-  LoroMovableList<string>
+export const SpaceSidebarNavigation = defComponent(
+  "spaceSidebarNavigation:01JPFMBD0XT7B922PSQ7KQ99EW",
+  LoroMovableList<EntityIdStr>
 );
